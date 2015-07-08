d9e5e202d68384222bfb9976bcc1778f062d35d3
sai <sai1@gmail.com>
2015-07-08 15:09:53 -0500

    test github conn

---
+++ GitHubTest.java
@@ -0,0 +1,31 @@
+package org.eclipse.egit.github.core.client;
+
+
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
+public class GitHubTest {
+
+	public static void main(String[] args) {
+		try{
+
+		GitHubClient client = new GitHubClient();
+		//client.setCredentials("saiy1", "password");
+		System.out.println("authentication success");
+
+
+		RepositoryService service = new RepositoryService();
+		for (Repository repo : service.getRepositories("saiy1"))
+		{
+			System.out.println("owner="+repo.getOwner().getLogin());
+		  System.out.println("reponame="+repo.getName());
+		  System.out.println(" Watchers: " + repo.getWatchers());
+		}
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+}
